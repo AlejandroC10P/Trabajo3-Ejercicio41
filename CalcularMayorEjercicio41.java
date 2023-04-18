3dce726d6128b08df855e604489af5fa41d2fecc
Alejandro Castro <111087081+AlejandroC10P@users.noreply.github.com>
2023-04-17 21:55:09 -0500

    Add files via upload

---
+++ CalcularMayorEjercicio41.java
@@ -0,0 +1,13 @@
+package Capitulo_4;
+
+public class CalcularMayorEjercicio41 {
+    public static double calcularMayor(double numero[]){
+        double mayor = numero[0];
+        for (int i = 1; i < numero.length; i++){
+            if(numero[i] > mayor){
+                mayor = numero[i];
+            }
+        }
+        return mayor;
+    }
+}
---
+++ Ejercicio41.java
@@ -0,0 +1,8 @@
+package Capitulo_4;
+
+public class Ejercicio41 {
+    public static void main(String[] args) {
+        InterfazEjercicio41 form = new InterfazEjercicio41();
+        form.setVisible(true);
+    }
+}
